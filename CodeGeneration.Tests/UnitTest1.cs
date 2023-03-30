48f2a5185529a441623058fdb64b88ad15560e0b
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2023-03-30 09:45:39 +0700

    add some tests

--- CodeGeneration.Tests/UnitTest1.cs
+++ CodeGeneration.Tests/UnitTest1.cs
@@ -1,9 +1,34 @@
+using NJsonSchema.CodeGeneration.TypeScript;
+using NSwag;
+using NSwag.CodeGeneration.TypeScript;
+
 namespace CodeGeneration.Tests;
 
+[UsesVerify]
 public class UnitTest1
 {
+
     [Fact]
-    public void Test1()
+    public async Task Tst1()
     {
+        // Arrange
+        var document = await OpenApiDocument.FromFileAsync("test1.json");
+        var clientGenerator = new TypeScriptClientGenerator(
+            document,
+            new TypeScriptClientGeneratorSettings
+            {
+                TypeScriptGeneratorSettings =
+                {
+                    TypeScriptVersion = 4.3m,
+                    NullValue = TypeScriptNullValue.Undefined
+                }
+            }
+        );
+
+        // Act
+        var code = clientGenerator.GenerateFile();
+
+        // Assert
+        await VerifyHelper.Verify(code);
     }
 }
---
+++ CodeGeneration.Tests/VerifyHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeGeneration.Tests;
+
+public static class VerifyHelper
+{
+    /// <summary>
+    /// Helper to run verify tests with sane defaults.
+    /// </summary>
+    public static Task Verify(string output, Func<SettingsTask, SettingsTask>? action = null)
+    {
+        var settings = Verifier
+            .Verify(output)
+            .ScrubLinesContaining(
+                StringComparison.OrdinalIgnoreCase,
+                "Generated using the NSwag toolchain"
+            )
+            .UseDirectory("Snapshots");
+
+        if (action != null)
+            settings = action.Invoke(settings);
+        return settings.ToTask();
+    }
+}
