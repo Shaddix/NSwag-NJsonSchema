f2abf9b418b080419d91eec755b0bd12249bf0f1
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2023-04-02 22:08:12 +0700

    prepare npm package, add build scripts

---
+++ NSwag.Console/Program.cs
@@ -0,0 +1,6 @@
+﻿using NSwag;
+using NSwag.Commands;
+
+Console.Write("NSwag command line tool for .NET Core " + RuntimeUtilities.CurrentRuntime + ", ");
+var processor = new NSwagCommandProcessor(new CoreConsoleHost());
+return await processor.ProcessAsync(args);
