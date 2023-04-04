08e13b82cc10d55d861c8109c9130bebe01cc0aa
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2023-04-04 08:58:55 +0700

    fix permissions issue on mac/linux

--- nswag-portable.js
+++ nswag-portable.js
@@ -6,7 +6,11 @@ process.title = 'nswag';
 console.log("NSwag NPM CLI");
 var args = process.argv.splice(2, process.argv.length - 2).map(function (a) { return a.indexOf(" ") === -1 ? a : '"' + a + '"' }).join(" ");
 
+const nswagPath = __dirname + '/bin/nswag-portable';
+const fs = require('fs');
+fs.chmodSync(nswagPath, '755');
+
 var c = require('child_process');
-var cmd = '"' + __dirname + '/bin/nswag-portable" ' + args;
+var cmd = '"' + nswagPath + '" ' + args;
 var code = c.execSync(cmd, { stdio: [0, 1, 2] });
 return;
