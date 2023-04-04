82347f341187acca0e039eff65b2f7b507f314c5
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2023-04-04 23:30:45 +0700

    build for mac

--- nswag-portable.js
+++ nswag-portable.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 "use strict";
+const os = require('os');
 
 // Initialize
 process.title = 'nswag';
 console.log("NSwag NPM CLI");
 var args = process.argv.splice(2, process.argv.length - 2).map(function (a) { return a.indexOf(" ") === -1 ? a : '"' + a + '"' }).join(" ");
 
-const nswagPath = __dirname + '/bin/nswag-portable';
+const isMacOs = os.platform() === 'darwin';
+const nswagPath = __dirname + '/bin/nswag-portable' + isMacOs ? '.mac' : '';
 const fs = require('fs');
 fs.chmodSync(nswagPath, '755');
 
