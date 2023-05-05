0a5893732293e1783dce109912d9eab80d3fd1b9
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2023-05-05 21:46:48 +0700

    minor refactoring, split between files

---
+++ DemoSwagger/Controllers/QueryParamsController.cs
@@ -0,0 +1,19 @@
+﻿using DemoSwagger;
+using DemoSwagger.FromQueryJson;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("query-params")]
+[ApiController]
+
+public class QueryParamsController
+{
+    [HttpGet("One")]
+    public void One([FromQuery]DummyDto param)
+    {
+    }
+    
+    [HttpGet("JsonInQueryParams")]
+    public void JsonInQueryParams([FromJsonQuery]DummyDto param)
+    {
+    }
+}
